69215248be7c5c16c5e276951ae71bd6545cc7ee
acousma82 <Sebastian_Peter@web.de>
2018-01-09 22:17:51 +0100

    fixed the login/logout link transformation to dropdownmenu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
     before_save { self.email = email.downcase }
+
     
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -11,7 +12,8 @@ class User < ApplicationRecord
     # Adds methods to set and authenticate against a BCrypt password. This mechanism requires you to have a password_digest attribute.
     #fügt die authenticate Methode für den user hinzu so das
     #man mit Hilfe von user.authenticate("password") herausfinden kann ob das eingegebene Passwort stimmt.
-    #fügt in diesem Zusammenhang die virtuellen attribute password und password_confirmation hinzu inklusive validierungen und methoden dass diese beiden einander gleichen müssen.
+    #fügt in diesem Zusammenhang die virtuellen attribute password und password_confirmation hinzu inklusive validierungen und methoden dass 
+    #diese beiden einander gleichen müssen.
     has_secure_password  
     # allow nil true weil has secure password schaut ob der benutzer ein passwort hat das nicht leer ist 
     #und damit man beim updaten eines Users das Passwortfeld leer lässt.
