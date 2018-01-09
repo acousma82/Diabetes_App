540985d24b5377b3d428cfdb59d30ddbf79c5e10
Admin <Sebastian_Peter@web.de>
2018-01-09 14:50:36 +0100

    Basic login WIP

--- app/assets/javascripts/Bolus_Rechner.js
+++ app/assets/javascripts/Bolus_Rechner.js
@@ -11,7 +11,7 @@ function time() {
     let minutes = currentMinutes()
     if (hours < 10) { hours = '0' + hours; }
     if (minutes < 10) { minutes = '0' + minutes; }
-    return (hours + ":" + minutes + "Uhr");
+    return (hours + ":" + minutes + " Uhr");
 }
 
 function showTime(){
