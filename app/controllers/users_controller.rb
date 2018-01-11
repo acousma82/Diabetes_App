8409a039f85dd76e1184f1dc5380fc4374dffd4c
Admin <Sebastian_Peter@web.de>
2018-01-11 15:36:33 +0100

    Admin Index test fixed. Admin is allowed to view unactivated user on index but not their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     
     #User.paginate(available via the will-paginate gem) pulls the users out of the database one chunk at a time (30 by default), based on the :page parameter. So, for example, page 1 is users 1–30, page 2 is users 31–60, etc. If page is nil, paginate simply returns the first page.Here the page parameter comes from params[:page], which is generated automatically by will_paginate inside the view.
   def index
-    @users = User.where(activated: true).paginate(page: params[:page]) 
+    @users = User.paginate(page: params[:page]) 
   end
   
   def new
