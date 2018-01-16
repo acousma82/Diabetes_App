e6dda206aaaf72d2a79bf426e18e45bb37e11fc2
Admin <Sebastian_Peter@web.de>
2018-01-16 15:14:37 +0100

    added form, view and action to create be-factors for a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,14 @@ class UsersController < ApplicationController
     end
   end
   
+  def bolus
+  end
+
+  def edit_diabsettings
+    @user = User.find(params[:id])
+  end
+  
+
 
   
 
@@ -56,11 +64,30 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
+
 private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation, :admin)
+                                   :password_confirmation, :zero_till_one, 
+                                   :one_till_two, :two_till_three,
+                                   :three_till_four, :four_till_five, :five_till_six,
+                                   :six_till_seven, :seven_till_eight, :eight_till_nine,
+                                   :nine_till_ten,
+                                   :ten_till_eleven,
+                                   :eleven_till_twelve,
+                                   :twelve_till_thirteen,
+                                   :thirteen_till_fourteen,
+                                   :fourteen_till_fifteen,
+                                   :fifteen_till_sixteen,
+                                   :sixteen_till_seventeen,
+                                   :seventeen_till_eightteen,
+                                   :eightteen_till_nineteen,
+                                   :nineteen_till_twenty,
+                                   :twenty_till_twentyone,
+                                   :twentyone_till_twentytwo,
+                                   :twentytwo_till_twentythree,
+                                   :twentythree_till_zero )
     end
 
       # Before filters
