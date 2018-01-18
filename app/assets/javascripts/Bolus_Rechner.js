c6665d1ba3315ba5e358025dc7c57b5c6bdde7af
acousma82 <Sebastian_Peter@web.de>
2018-01-18 18:59:32 +0100

    finished connecting Bu factors from Database with bolus calculator

--- app/assets/javascripts/Bolus_Rechner.js
+++ app/assets/javascripts/Bolus_Rechner.js
@@ -1,3 +1,27 @@
+
+function currentMinutes(){
+    let date = new Date();
+    return date.getMinutes();
+}
+function currentHours(){
+    let date = new Date();
+    return date.getHours();
+}
+function time() {
+    let hours = currentHours()
+    let minutes = currentMinutes()
+    if (hours < 10) { hours = '0' + hours; }
+    if (minutes < 10) { minutes = '0' + minutes; }
+    return (hours + ":" + minutes + " Uhr");
+}
+
+function showTime(){
+    let uHr = document.getElementById("Uhrzeit");
+    //checking if the id exists on the page
+    if (uHr === null){return}
+    uHr.innerHTML = "Es ist " + time() + ".";
+};
+
 function currentMinutes(){
     let date = new Date();
     return date.getMinutes();
@@ -25,36 +49,90 @@ function timeFakt() {
     showTime();
    
     bu_json = gon.bu_factors
-    if (bu_json === null){return};
+    if (bu_json == null) {return};
     let factorJson = JSON.parse(gon.bu_factors);
 
     let hours = currentHours();
-    let beFactorNight = 0.5; 
-    let beFactorMorning = 1.3;
-    let beFactorNoon = 0.8;
-    let beFactorEvening = 0.5;
+    
 
     let beFactor = document.getElementById("faktor");
     //checking if the id exists on the page
     if (beFactor === null){return}
-    if (hours > 20 || hours < 6) {
-     beFactor.value = beFactorNight;
-     
-    }
-    else if (hours >= 6 && hours < 11) {
-     beFactor.value = beFactorMorning;
+    switch (hours) {
+        case 0:
+            beFactor.value = factorJson.zero_till_one;
+            break;
+        case 1:
+            beFactor.value = factorJson.one_till_two;
+            break;
+        case 2:
+            beFactor.value = factorJson.two_till_three;
+            break;
+        case 3:
+            beFactor.value = factorJson.three_till_four;
+            break;
+        case 4:
+            beFactor.value = factorJson.four_till_five;
+            break;
+        case 5:
+            beFactor.value = factorJson.five_till_six;
+            break;
+        case 6:
+            beFactor.value = factorJson.six_till_seven;
+            break;
+        case 7:
+            beFactor.value = factorJson.seven_till_eight;
+            break;
+        case 8:
+            beFactor.value = factorJson.eight_till_nine;
+            break;
+        case 9:
+            beFactor.value = factorJson.nine_till_ten;
+            break;
+        case 10:
+            beFactor.value = factorJson.ten_till_eleven;
+            break;
+        case 11:
+            beFactor.value = factorJson.eleven_till_twelve;
+            break;
+        case 12:
+            beFactor.value = factorJson.twelve_till_thirteen;
+            break;
+        case 13:
+            beFactor.value = factorJson.thirteen_till_fourteen;
+            break;
+        case 14:
+            beFactor.value = factorJson.fourteen_till_fifteen;
+            break;
+        case 15:
+            beFactor.value = factorJson.fifteen_till_sixteen;
+            break;
+        case 16:
+            beFactor.value = factorJson.sixteen_till_seventeen;
+            break;
+        case 17:
+            beFactor.value = factorJson.seventeen_till_eightteen;
+            break;
+        case 18:
+            beFactor.value = factorJson.eightteen_till_nineteen;
+            break;
+        case 19:
+            beFactor.value = factorJson.ninetteen_till_twenty;
+            break;
+        case 20:
+            beFactor.value = factorJson.twenty_till_twentyone;
+            break;
+        case 21:
+            beFactor.value = factorJson.twentyone_till_twentytwo;
+            break;
+        case 22:
+            beFactor.value = factorJson.twentytwo_till_twentythree;
+            break;
+        case 23:
+            beFactor.value = factorJson.twentythree_till_zero;
+            break;
+      }
     
-    }
- 
-    else if (hours === 15) {
-     beFactor.value = factorJson.fifteen_till_sixteen;
-     
-    }
-    else if (hours >= 18 && hours <= 20) {
-        beFactor.value = beFactorEvening;
-     
- }
-
 };
 window.addEventListener("turbolinks:load", timeFakt);
 
@@ -99,5 +177,4 @@ function insulinBerechnen() {
     };
 
 };
-
 
