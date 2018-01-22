35ece1363846ad476b867df5f41530c3d039e3dd
Admin <Sebastian_Peter@web.de>
2018-01-22 15:50:47 +0100

    finished showing Diary Entries inside a continous table

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -1,30 +1,31 @@
-class DiaryEntryController < ApplicationController
-
-def new
-end
-
-def create
-    @diary_entry = current_user.diary_entries.build(diary_entry_params)
-    if @diary_entry.save
-        flash.now[:success] = "A new Diary entry was created!"
-        redirect_to users_bolus_path
-    else
-        flash.now[:danger] = "Diary entry could not be created!"
-        render 'new'
+class DiaryEntriesController < ApplicationController
+    
+    def new
     end
-end
-
-def update
-end
-
-def destroy
-end
-
-private
-
-def diary_entry_params
-    params.require(:diary_entry).permit(:comment, :insulin, :bloodsugar, :carbohydrates)
-end
-
-end
-
+    
+    def create
+        @diary_entry = current_user.diary_entries.build(diary_entry_params)
+        if @diary_entry.save
+            flash.now[:success] = "A new Diary entry was created!"
+            redirect_to current_user
+        else
+            flash.now[:danger] = "Diary entry could not be created!"
+            render 'new'
+        end
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    private
+    
+    def diary_entry_params
+        params.require(:diary_entry).permit(:comment, :insulin, :bloodsugar, :carbohydrates)
+    end
+    
+    end
+    
+    
--- app/models/diary_entry.rb
+++ app/models/diary_entry.rb
@@ -1,7 +1,7 @@
 class DiaryEntry < ApplicationRecord
   #bedeutet das jeder Tagebucheintrag einem User zugeordnet wird. Bei der Erstellung eines neuen entry wir geschaut ob es eine user_id gibt und ob es einen user gibt der diese id hat
   belongs_to :user
-  default_scope -> { order(created_at: :desc)}
+  default_scope -> { order(created_at: :asc)}
   validates :user_id, presence: true
   validate :has_content
 
