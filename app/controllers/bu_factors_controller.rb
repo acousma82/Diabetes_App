42ebaa1adcc6cff7acf80215fcdce9bfe82f909f
acousma82 <Sebastian_Peter@web.de>
2018-02-03 12:21:58 +0100

    fixed _error_messages_partial to display errors for non valid  bu_factors

--- app/controllers/bu_factors_controller.rb
+++ app/controllers/bu_factors_controller.rb
@@ -12,6 +12,7 @@ class BuFactorsController < ApplicationController
     end
   end
 
+
   private
   def bu_factor_params
     params.require(:bu_factor).permit(:zero_till_one, :one_till_two, :two_till_three, :three_till_four, :four_till_five, :five_till_six, :six_till_seven, :seven_till_eight, :eight_till_nine, :nine_till_ten, :ten_till_eleven, :eleven_till_twelve, :twelve_till_thirteen, :thirteen_till_fourteen, :fourteen_till_fifteen, :fifteen_till_sixteen, :sixteen_till_seventeen, :seventeen_till_eightteen, :eightteen_till_nineteen, :nineteen_till_twenty, :twenty_till_twentyone, :twentyone_till_twentytwo, :twentytwo_till_twentythree, :twentythree_till_zero)
